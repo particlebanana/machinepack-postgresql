5b23900eecbdb01a1237a89294f550468ee3b1b2
Cody Stoltman <particlebanana@gmail.com>
2016-03-01 12:33:09 -0600

    expand constants

--- machines/rollback-transaction.js
+++ machines/rollback-transaction.js
@@ -12,11 +12,20 @@ module.exports = {
 
   inputs: {
 
-    connection:
-      require('../constants/connection.input'),
+    connection: {
+      friendlyName: 'Connection',
+      description: 'An active database connection.',
+      extendedDescription: 'The provided database connection instance must still be active.  Only database connection instances created by the `getConnection()` machine in this driver are supported.',
+      example: '===',
+      required: true
+    },
 
-    meta:
-      require('../constants/meta.input')
+    meta: {
+      friendlyName: 'Meta (custom)',
+      description: 'Additional stuff to pass to the driver.',
+      extendedDescription: 'This is reserved for custom driver-specific extensions.  Please refer to the documentation for the driver you are using for more specific information.',
+      example: '==='
+    }
 
   },
 
@@ -33,8 +42,17 @@ module.exports = {
       }
     },
 
-    badConnection:
-      require('../constants/badConnection.exit')
+    badConnection: {
+      friendlyName: 'Bad connection',
+      description: 'The provided connection is not valid or no longer active.  Are you sure it was obtained by calling this driver\'s `getConnection()` method?',
+      extendedDescription: 'Usually, this means the connection to the database was lost due to a logic error or timing issue in userland code.  In production, this can mean that the database became overwhelemed or was shut off while some business logic was in progress.',
+      outputVariableName: 'report',
+      outputDescription: 'The `error` property is a JavaScript Error instance containing the raw error from the database.  The `meta` property is reserved for custom driver-specific extensions.',
+      example: {
+        error: '===',
+        meta: '==='
+      }
+    }
 
   },
 
